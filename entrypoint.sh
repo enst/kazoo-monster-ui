6a0e90360403b0e7a18bd60fa93a67bbe852e06c
Bing Li <enst.bupt@gmail.com>
2015-08-05 20:07:47 -0400

    config.js

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+MY_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
+sed -i "/api.zswitch.net/s/api.zswitch.net/$MY_IP/" /var/www/html/monster-ui/js/config.js
+sed -i "/api.zswitch.net/s/https:\/\/api.zswitch.net:8443/http:\/\/$MY_IP:8000/" /var/www/html/kazoo-ui/config/config.js
+
 service httpd restart
 
 exec /bin/bash "$@"
